0f78e16cd64cf3545abca44ef411e697c2b1e08c
Vzmind <vzmind@gmail.com>
2011-09-18 16:57:27 +0200

    Display a custom Object stored in database.com

---
+++ app/controllers/visitors.rb
@@ -0,0 +1,18 @@
+SalesforcePadrino.controllers :leads do
+
+  before do
+    if ENV['sfdc_token'].nil?
+      redirect "/auth/forcedotcom"
+    end 
+  end
+
+  get :index, :map => '/visitors' do
+    @leads = cache( "cache_for_visitors", :expires_in => 15 ) do
+      @leads = SalesforceConnector::Visitor.get_first_hundred
+    end
+
+    render "visitor/index"
+  end  
+
+  
+end
